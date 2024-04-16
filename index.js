8784e98a39048c7c3670f8b174f134010b64eb10
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-04-16 16:39:27 +0200

    Finished Project

--- index.js
+++ index.js
@@ -10,15 +10,25 @@ let API_KEY3 = "https://pokeapi.co/api/v2/pokemon/";
 //Button
 let startBtn = document.querySelector("#start");
 let startBtn2 = document.querySelector("#start2");
+let compareButton = document.querySelector("#compareBtn");
+let battleButton = document.querySelector("#battleBtn");
+
 //Select
 let pokeSelect = document.querySelector("#pokeSelect");
 pokeSelect.style.textTransform = "capitalize";
 let pokeSelect2 = document.querySelector("#pokeSelect2");
 pokeSelect2.style.textTransform = "capitalize";
 //Containers
+let pokeWrap = document.querySelector("#pokeWrap");
 let pokeContainer = document.querySelector("#poke-container");
 let pokeCardOne = document.querySelector("#pokecard-one");
 let pokeCardTwo = document.querySelector("#pokecard-two");
+let interactContainer = document.querySelector("#interact");
+let compareResults = document.querySelector("#compareResults");
+compareResults.style.display = "none";
+let buttonContainer = document.querySelector("#buttonContainer");
+buttonContainer.style.display = "none";
+
 //Arrays
 let pokemonArray = [];
 let cardArray = [];
@@ -31,6 +41,91 @@ class Pokemon {
     this.weight = weight;
     this.height = height;
     this.stats = stats;
+  }
+  compare(otherPokemon) {
+    let comparison = [];
+    if (this.weight > otherPokemon.weight) {
+      //COMPARE WEIGHT
+      comparison.push(`${this.name} is heavier than ${otherPokemon.name}`);
+    } else {
+      comparison.push(`${otherPokemon.name} is heavier than ${this.name}`);
+    } //COMPARE HEIGHT
+    if (this.height > otherPokemon.height) {
+      comparison.push(`${this.name} is taller than ${otherPokemon.name}`);
+    } else {
+      comparison.push(`${otherPokemon.name} is taller than ${this.name}`);
+    } //COMPARE HEALTH
+    if (this.stats[0].base_stat > otherPokemon.stats[0].base_stat) {
+      comparison.push(
+        `${this.name} got greater health than ${otherPokemon.name}`
+      );
+    } else if (this.stats[0].base_stat === otherPokemon.stats[0].base_stat) {
+      comparison.push(`${this.name} health is equal ${otherPokemon.name}`);
+    } else {
+      comparison.push(
+        `${otherPokemon.name} got greater health than ${this.name}`
+      );
+    } //COMPARE ATTACK
+    if (this.stats[1].base_stat > otherPokemon.stats[1].base_stat) {
+      comparison.push(
+        `${this.name} has greater attack damage than ${otherPokemon.name}`
+      );
+    } else {
+      comparison.push(
+        `${otherPokemon.name} has greater attack damage than ${this.name}`
+      );
+    } //COMPARE DEFENSE
+    if (this.stats[2].base_stat > otherPokemon.stats[2].base_stat) {
+      comparison.push(
+        `${this.name} has greater defense than ${otherPokemon.name}`
+      );
+    } else {
+      comparison.push(
+        `${otherPokemon.name} has greater defense than ${this.name}`
+      );
+    } //COMPARE SPEED
+    if (this.stats[5].base_stat > otherPokemon.stats[5].base_stat) {
+      comparison.push(`${this.name} is faster than ${otherPokemon.name}`);
+    } else {
+      comparison.push(`${otherPokemon.name} is faster than ${this.name}`);
+    }
+    return comparison;
+  }
+  attack(defender) {
+    let attack = this.stats[1].base_stat + this.stats[3].base_stat;
+    let defense = defender.stats[2].base_stat + defender.stats[4].base_stat;
+    let damage = Math.floor((attack - defense) * 0.8);
+    if (damage < 10) {
+      defender.hp -= 10;
+      return 10;
+    } else {
+      defender.hp -= damage;
+      return damage;
+    }
+  }
+  battle(opponent) {
+    let damageDealt = this.attack(opponent);
+    let battleLog = `${this.name} attacks ${opponent.name} and deals ${damageDealt} damage.`;
+    console.log(battleLog);
+    this.removeHp(damageDealt, opponent);
+    return battleLog;
+  }
+  //add remove health
+  removeHp(damageDealt, opponent) {
+    let defender = document.querySelector(`#${opponent.name}`);
+    defender.value -= damageDealt;
+    if (defender.value <= 0) {
+      setTimeout(() => {
+        compareResults.innerHTML = "";
+        let firstLine = document.createElement("h3");
+        firstLine.innerText = opponent.name + " is slain";
+        //Lägg till quote?
+        let secondLine = document.createElement("p");
+        secondLine.innerText =
+          "All these moments will be lost in time, like tears, in rain, time to die";
+        compareResults.append(firstLine, secondLine);
+      }, 2000);
+    }
   }
 }
 
@@ -54,24 +149,19 @@ createDropdown(pokeSelect);
 createDropdown(pokeSelect2);
 
 //Start making things happen
-
+//Buttons
 startBtn.addEventListener("click", () => {
+  compareResults.style.display = "none";
+  compareResults.innerHTML = "";
   let selectedPokemon = pokeSelect.value;
-  if (cardArray.length < 2) {
-    createPokemon(selectedPokemon, pokeCardOne);
-  } else {
-    cardArray.shift();
-    createPokemon(selectedPokemon, pokeCardOne);
-  }
+  createPokemon(selectedPokemon, pokeCardOne);
 });
+
 startBtn2.addEventListener("click", () => {
+  compareResults.style.display = "none";
+  compareResults.innerHTML = "";
   let selectedPokemon = pokeSelect2.value;
-  if (cardArray.length < 2) {
-    createPokemon(selectedPokemon, pokeCardTwo);
-  } else {
-    cardArray.shift();
-    createPokemon(selectedPokemon, pokeCardTwo);
-  }
+  createPokemon(selectedPokemon, pokeCardTwo);
 });
 
 //Create Pokemon! Getting information from the api and passing it to the create pokeCard function
@@ -79,6 +169,7 @@ let createPokemon = async (pokemonUrl, targetContainer) => {
   let response = await axios.get(pokemonUrl);
   let pokemonDetails = response.data;
   console.log(pokemonDetails.name);
+  console.log(pokemonDetails);
   createPokeCard(pokemonDetails, targetContainer); //Sending all the pokemonDetails to CreatePokeCard and pushing to DOM
 };
 
@@ -149,13 +240,10 @@ let createPokeCard = (pokemonDetails, targetContainer) => {
   let weightHeightDiv = document.createElement("div");
   weightHeightDiv.classList.add("weightHeight");
 
-  // paragraphs
-  //   let pokeTypeDesc = document.createElement("p")
-  //   pokeTypeDesc.innerText =
   let pokeWeight = document.createElement("p");
-  pokeWeight.innerText = "Weight: " + pokemonDetails.weight + "lbs.";
+  pokeWeight.innerText = "Weight: " + pokemonDetails.weight + " lbs.";
   let pokeHeight = document.createElement("p");
-  pokeHeight.innerText = "Height: " + pokemonDetails.height + "feet.";
+  pokeHeight.innerText = "Height: " + pokemonDetails.height + " feet.";
   weightHeightDiv.append(pokeWeight, pokeType, pokeHeight);
 
   // CONTAINER FOR STATS (PROGRESS BAR AND LABEL)
@@ -213,6 +301,119 @@ let createPokeCard = (pokemonDetails, targetContainer) => {
     speedProgress
   );
   targetContainer.append(pokeName, pokeImg, weightHeightDiv, statsContainer);
-  cardArray.push(newPokemon);
+  if (targetContainer === pokeCardOne) {
+    cardArray.splice(0, 1, newPokemon); // börjar på 0, hur många steg tar vi bort och vad lägger vi till
+  } else {
+    cardArray.splice(1, 1, newPokemon);
+  }
   console.log(cardArray);
+  //Bryt ut. Give each card an index and compare function each other
+  if (cardArray.length === 2) {
+    buttonContainer.style.display = "flex";
+  }
+};
+
+//COMPARE POKEMONS
+compareButton.addEventListener("click", () => {
+  compareResults.style.display = "block";
+  compareResults.innerHTML = "";
+
+  interactContainer.append(compareResults);
+  if (cardArray.length >= 2) {
+    let firstPokemon = cardArray[0];
+    let secondPokemon = cardArray[1];
+    let comparisonArray = firstPokemon.compare(secondPokemon);
+    comparisonArray.forEach((string) => {
+      console.log(string);
+      let p = document.createElement("p");
+      p.innerText = string;
+      compareResults.append(p);
+    });
+  }
+});
+//BATTLE POKEMONS
+battleButton.addEventListener("click", () => {
+  compareResults.style.display = "block";
+  compareResults.innerHTML = "";
+  let firstPokemon = cardArray[0];
+  let secondPokemon = cardArray[1];
+  let battleDiv = document.createElement("div");
+  battleDiv.classList.add("battleDiv");
+  let battleLogContainer = document.createElement("div");
+  battleLogContainer.id = "battleLogContainer";
+  battleLogContainer.style.display = "none";
+  compareResults.append(battleDiv, battleLogContainer);
+
+  createBattleField(firstPokemon, secondPokemon, battleDiv);
+});
+
+//Creating the battle ground
+let createBattleField = (firstPokemon, secondPokemon, battleDiv) => {
+  let battleLogContainer = document.querySelector("#battleLogContainer");
+
+  let leftField = document.createElement("div");
+  leftField.classList.add("fighter");
+  let rightField = document.createElement("div");
+  rightField.classList.add("fighter");
+  let leftName = document.createElement("h3");
+  let rightName = document.createElement("h3");
+  let leftHP = document.createElement("progress");
+  let rightHP = document.createElement("progress");
+  let leftAttack = document.createElement("button");
+  let rightAttack = document.createElement("button");
+  if (firstPokemon.stats[5].base_stat > secondPokemon.stats[5].base_stat) {
+    rightAttack.classList.add("disableBtn");
+  } else {
+    leftAttack.classList.add("disableBtn");
+  }
+  leftName.innerText = firstPokemon.name;
+  rightName.innerText = secondPokemon.name;
+  leftHP.setAttribute("value", firstPokemon.stats[0].base_stat);
+  leftHP.setAttribute("max", firstPokemon.stats[0].base_stat);
+  leftHP.id = firstPokemon.name;
+  rightHP.setAttribute("value", secondPokemon.stats[0].base_stat);
+  rightHP.setAttribute("max", secondPokemon.stats[0].base_stat);
+  rightHP.id = secondPokemon.name;
+  leftAttack.innerText = "Attack";
+  rightAttack.innerText = "Attack";
+  leftField.append(leftName, leftHP, leftAttack);
+  rightField.append(rightName, rightHP, rightAttack);
+  battleDiv.append(leftField, rightField);
+
+  battleFunction(
+    leftAttack,
+    rightAttack,
+    firstPokemon,
+    secondPokemon,
+    battleLogContainer,
+    leftHP,
+    rightHP
+  );
+};
+
+//Function for battle and attacks
+let battleFunction = (
+  leftAttack,
+  rightAttack,
+  firstPokemon,
+  secondPokemon,
+  battleLogContainer
+) => {
+  leftAttack.addEventListener("click", () => {
+    battleLogContainer.style.display = "block";
+    battleLogContainer.innerHTML = "";
+    leftAttack.classList.toggle("disableBtn");
+    rightAttack.classList.toggle("disableBtn");
+    let battleLog = firstPokemon.battle(secondPokemon); // battle innehåller tre metoder
+    //battle(opponent) + this.attack(opponent) + removeHp(damageDealt, opponent)
+    battleLogContainer.innerText = battleLog; //battle innehåller battleLog
+  });
+  rightAttack.addEventListener("click", () => {
+    battleLogContainer.style.display = "block";
+    battleLogContainer.innerHTML = "";
+    rightAttack.classList.toggle("disableBtn");
+    leftAttack.classList.toggle("disableBtn");
+    let battleLog = secondPokemon.battle(firstPokemon); //battle innehåller battleLog
+    battleLogContainer.innerText = battleLog;
+  });
 };
